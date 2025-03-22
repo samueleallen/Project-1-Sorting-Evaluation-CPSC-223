e57556cca00a10a6b20dff4eb11d2d7bb4fc3caf
Alex-N-GU <aneyman@zagmail.gonzaga.edu>
2025-03-21 22:05:28 -0700

    Update test_sorting.cpp

--- test_sorting.cpp
+++ test_sorting.cpp
@@ -17,6 +17,48 @@ bool is_sorted(DLLNode* head) {
     return true;
 }
 
+bool test_merge_sort_dll() {
+    //Test cases: Empty list, size of 1 list, already sorted, not sorted, & lists with duplicate nums
+    DoublyLinkedList l1, l2, l3, l4, l5;
+
+    // List of size 0
+    l1.merge_sort();
+    assert(is_sorted(l1.get_head()));
+    
+    // List of size 1
+    l2.push_front(1);
+    l2.merge_sort();
+    assert(is_sorted(l2.get_head()));
+
+    // Already sorted list
+    l3.push_front(5);
+    l3.push_front(4);
+    l3.push_front(3);
+    l3.push_front(2);
+    l3.push_front(1);
+    l3.merge_sort();
+    assert(is_sorted(l3.get_head()));
+
+    // Unordered list
+    l3.push_front(10);
+    l3.push_front(5);
+    l3.push_front(13);
+    l3.push_front(17);
+    l3.push_front(11);
+    l3.merge_sort();
+    assert(is_sorted(l3.get_head()));
+
+    // // List with duplicate numbers
+    l3.push_front(10);
+    l3.push_front(10);
+    l3.push_front(11);
+    l3.push_front(11);
+    l3.push_front(11);
+    l3.merge_sort();
+    assert(is_sorted(l3.get_head()));
+    return true;
+}
+
 bool test_quick_sort_dll() {
     //Test cases: Empty list, size of 1 list, already sorted, not sorted, & lists with duplicate nums
     DoublyLinkedList l1, l2, l3, l4, l5;
